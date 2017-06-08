97f57bb380960c45f353244a12fbf6a618b48c9e
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-06-08 21:43:49 +0700

    Added Albums and Photos

--- app/src/main/java/com/sample/application/MainApplication.kt
+++ app/src/main/java/com/sample/application/MainApplication.kt
@@ -1,22 +1,16 @@
 package com.sample.application
 
-import android.app.Activity
 import android.app.Application
 import com.letv.sarrsdesktop.blockcanaryex.jrt.BlockCanaryEx
 import com.letv.sarrsdesktop.blockcanaryex.jrt.Config
 import com.squareup.leakcanary.LeakCanary
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
 import io.paperdb.Paper
-import javax.inject.Inject
 
 /**
  * Created by tomykho on 5/16/17.
  */
 
-class MainApplication : Application(), HasActivityInjector {
-
-    @Inject lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+class MainApplication : Application() {
 
     override fun onCreate() {
         if (BlockCanaryEx.isInSamplerProcess(this)) {
@@ -26,14 +20,12 @@ class MainApplication : Application(), HasActivityInjector {
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return
         }
-        DaggerAppComponent.create().inject(this)
+        // Initialization
         Paper.init(this)
 
         // BlockCanaryEx and LeakCanary must be last
         BlockCanaryEx.install(Config(this))
         LeakCanary.install(this)
     }
-
-    override fun activityInjector() = activityInjector
 
 }
