4f83366990aed6136abe0efbef782c7cb6665193
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-06-15 15:13:10 +0700

    Changed package name

--- app/src/androidTest/java/com/sample/application/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sample/application/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.sample.application;
+package com.boilerplate;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sample.application", appContext.getPackageName());
+        assertEquals("com.boilerplate", appContext.getPackageName());
     }
 }
