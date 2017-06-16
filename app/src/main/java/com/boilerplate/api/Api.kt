50e50994f215caed266c674b1f8a8a4d55c28776
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-06-16 18:31:39 +0700

    Changed Jackson to GSON

--- app/src/main/java/com/boilerplate/api/Api.kt
+++ app/src/main/java/com/boilerplate/api/Api.kt
@@ -1,19 +1,15 @@
 package com.boilerplate.api
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
 import retrofit2.Retrofit
-import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
 
     val service: ApiService by lazy {
-        val objectMapper = ObjectMapper()
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com")
                 .addCallAdapterFactory(ApiCallAdapter.Factory.create())
-                .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
         retrofit.create(ApiService::class.java)
     }
