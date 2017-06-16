db2038d7122990f871bb5ede8bd082258638a4a2
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-06-16 17:48:47 +0700

    Changed Fastjson to Jackson

--- app/src/main/java/com/boilerplate/api/Api.kt
+++ app/src/main/java/com/boilerplate/api/Api.kt
@@ -1,15 +1,19 @@
 package com.boilerplate.api
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import retrofit2.Retrofit
-import retrofit2.converter.fastjson.FastJsonConverterFactory
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 object Api {
 
     val service: ApiService by lazy {
+        val objectMapper = ObjectMapper()
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com")
                 .addCallAdapterFactory(ApiCallAdapter.Factory.create())
-                .addConverterFactory(FastJsonConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .build()
         retrofit.create(ApiService::class.java)
     }
